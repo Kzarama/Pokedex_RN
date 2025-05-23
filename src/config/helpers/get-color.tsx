0c01cf7a7eac822fab645bff9b4c2df5d9c24b6a
kevin zarama <kevin.zarama@pragma.com.co>
2025-05-23 15:32:11 -0500

    get color function changes

--- src/config/helpers/get-color.tsx
+++ src/config/helpers/get-color.tsx
@@ -1,20 +1,17 @@
-import ImageColors from "react-native-image-colors";
+import axios from "axios";
 
-export const getColorFromImage = async (image: string) => {
-	const fallbackColor = "grey";
-
-	const colors = await ImageColors.getColors(image, {
-		fallback: fallbackColor,
-	});
-
-	switch (colors.platform) {
-		case "android":
-			return colors.dominant ?? fallbackColor;
-
-		case "ios":
-			return colors.background ?? fallbackColor;
-
-		default:
-			return fallbackColor;
+export async function getColorFromImage(imageUrl: string): Promise<string> {
+	try {
+		const response = await axios.get(`https://api.microlink.io`, {
+			params: {
+				url: imageUrl,
+				palette: true,
+			},
+		});
+		const color = response.data.data.image.palette[0];
+		return typeof color === "string" ? color : "";
+	} catch (error) {
+		console.error("Error getting color from image", error);
+		return "";
 	}
-};
+}
